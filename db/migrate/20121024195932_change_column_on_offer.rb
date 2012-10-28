2156c3b1b36645af918ba37f5a579e97f5006b04
Erlendur Eiriksson <erlendure@gmail.com>
2012-10-27 23:03:36 -0500

    Change Migration

--- db/migrate/20121024195932_change_column_on_offer.rb
+++ db/migrate/20121024195932_change_column_on_offer.rb
@@ -1,9 +1,12 @@
 class ChangeColumnOnOffer < ActiveRecord::Migration
-  def self.up
-   change_column :offers, :featured, :boolean
+  def up
+    remove_column :offers, :featured
+    add_column :offers, :featured, :boolean    
   end
 
-  def self.down
-   change_column :offers, :featured, :string
+  def down
+    remove_column :offers, :featured  	
+    add_column :offers, :featured, :string
   end
 end
+
