0342677b7c6545a7a8fd558c69c26fe2f3216be1
Erlendur Eiriksson <erlendure@gmail.com>
2012-10-29 16:48:00 -0500

    More front page stuff

--- app/controllers/consumer_controller.rb
+++ app/controllers/consumer_controller.rb
@@ -1,5 +1,7 @@
 class ConsumerController < ApplicationController
   def show
   	@offers = Offer.all
+  	#@bullets = BulletPoint.all
+  	@bullets = BulletPoint.find_all_by_offer_id(@offers[0].id)
   end  
 end
