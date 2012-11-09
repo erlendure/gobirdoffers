2adcb12a95b9826c0bc5698b0337f367a6e10ef0
Erlendur Eiriksson <erlendure@gmail.com>
2012-11-09 15:57:35 -0600

    Test Layout2

--- app/controllers/consumer_controller.rb
+++ app/controllers/consumer_controller.rb
@@ -1,12 +1,17 @@
 class ConsumerController < ApplicationController
   def show
     @par_offer_id = params[:format]
+	@all = 0
   	if @par_offer_id.nil?
   	  @featured = Offer.first
   	  @offers = Offer.limit(12).offset(1)
-	else 
+	elsif @par_offer_id != '0'
   	  @featured = Offer.find(@par_offer_id) 		
   	  @offers = Offer.where('id != ?',@par_offer_id)
+	else 
+  	  @featured = Offer.first
+  	  @offers = Offer.limit(60)
+	  @all = 1
   	end
 
   	@bullets = BulletPoint.find_all_by_offer_id(@featured.id)
