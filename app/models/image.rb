76b231aafc0ac07f978cde72be08a097f5927aae
Erlendur Eiriksson <erlendure@gmail.com>
2012-12-26 14:23:14 -0600

    Review3

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,8 +11,13 @@
 
 class Image < ActiveRecord::Base
   attr_accessible :image_url, :offer_id, :photo
- 
+
+  has_attached_file :photo
+  
   belongs_to :offer
   
   validates :offer_id, presence: true
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes  
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/pjpeg', 'image/x-png']
 end
