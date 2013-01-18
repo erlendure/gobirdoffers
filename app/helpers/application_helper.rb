180115de4e461007861fd96690577ed7bf294e46
Erlendur Eiriksson <erlendure@gmail.com>
2013-01-17 22:22:18 -0600

    New Logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,21 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Reykjavik Offers"
+    url = request.url	
+    base_title = "Iceland Offers"
+	if url["icelandoffers.com"].nil? then base_title = "Reykjavik Offers" end
     if page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
     end
+  end
+  
+  #Returns logo
+  def decide_logo()
+    url = request.url
+    logo = "reykjavikoffers.gif"
+	if url["reykjaoffers.com"].nil? then logo = "icelandoffers.gif" end
+	logo
   end
 end
